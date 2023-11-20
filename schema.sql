ac2ba3ce2a9a6e2126e161f6609bb1e3bd7da4a6
jussitus <juzzitus@protonmail.com>
2023-11-20 16:23:04 +0200

    Modify schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,41 @@
 CREATE TABLE citations (
 	id SERIAL PRIMARY KEY,
-	book_name TEXT,
-	author TEXT);
+	type TEXT,
+	title TEXT,
+	year INTEGER
+);
+
+CREATE TABLE articles (
+	id INTEGER,
+	journal TEXT,
+	volume TEXT, /* these are strings in bibtex */
+	number TEXT,
+	pages TEXT,
+	FOREIGN KEY (id) REFERENCES citations(id)
+);
+
+CREATE TABLE books (
+	id INTEGER,
+	publisher TEXT,
+	address TEXT,
+	FOREIGN KEY (id) REFERENCES citations(id)	
+);
+
+CREATE TABLE inproceedings (
+	id INTEGER,
+	booktitle TEXT,
+	series TEXT,
+	pages TEXT,
+	publisher TEXT,
+	FOREIGN KEY (id) REFERENCES citations(id)	
+);
+
+CREATE TABLE authors (
+	id SERIAL PRIMARY KEY,
+	name TEXT
+);
+
+CREATE TABLE authors_citations (
+	author_id INTEGER REFERENCES authors,
+	citation_id INTEGER REFERENCES citations
+);
