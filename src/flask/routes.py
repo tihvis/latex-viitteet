48009ff1e4b9df05b228d0201e86a872ab1d40aa
Mira <mira.tihverainen@gmail.com>
2023-11-20 16:05:47 +0200

    Templatet ja routes.py siirretty flaskin alle

--- src/flask/routes.py
+++ src/flask/routes.py
@@ -1,10 +1,11 @@
+from app import app
 from flask import render_template, redirect, request, make_response, flash
 
 @app.route("/")
 def index():
-    return render_template("/index.html")
+    return render_template("index.html")
 
-@app.route("add_book", methods=["GET", "POST"])
+@app.route("/add_book", methods=["GET", "POST"])
 def add_book():
     if request.method =="GET":
         return render_template("add_book.html")
