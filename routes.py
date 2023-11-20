d6e5600b96df5451d3794f021ada3abe18b45d28
Olli Haapasalo <olli.haapasalo@hotmail.com>
2023-11-20 14:25:51 +0200

    routes.py update

--- routes.py
+++ routes.py
@@ -3,3 +3,10 @@ from flask import render_template, redirect, request, make_response, flash
 @app.route("/")
 def index():
     return render_template("/index.html")
+
+@app.route("add_book", methods=["GET", "POST"])
+def add_book():
+    if request.method =="GET":
+        return render_template("add_book.html")
+    #if request.method == "POST":
+
