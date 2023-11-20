1ef49202c0528cf2b682d1c37c0e05b207578d7b
Mira <mira.tihverainen@gmail.com>
2023-11-20 14:03:18 +0200

    Add postgresql setup

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
