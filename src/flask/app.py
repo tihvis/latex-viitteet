d2c921c976603153365e3e9b9b72e575bc6d7e9e
jussitus <juzzitus@protonmail.com>
2023-11-21 16:13:15 +0200

    Hotfix db url

--- src/flask/app.py
+++ src/flask/app.py
@@ -5,8 +5,8 @@ from sqlalchemy.sql import text
 
 app = Flask(__name__)
 import routes
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 #app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
