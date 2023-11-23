e9136660b18e8cc556ed1d8391485097d1cb61d8
rpessi <107867665+rpessi@users.noreply.github.com>
2023-11-23 18:18:19 +0200

    Korjattu run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,11 +1,12 @@
-poetry run python3 start-pg.sh &
-poetry run python3 src/flask/ &
+start-pg.sh &
+cd src/flask
+poetry run flask run &
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5001/ping)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
   do sleep 1;
 done
 
 poetry run robot src/tests
 status=$?
-kill $(lsof -t -i:5001)
+kill $(lsof -t -i:5000)
 exit $status
