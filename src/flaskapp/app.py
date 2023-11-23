e19cd2fa72ff7bfb3d7b82d0dd24767c0f8c13fa
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2023-11-23 18:37:07 +0200

    Moduuleihin lisätty luokat

---
+++ src/flaskapp/app.py
@@ -0,0 +1,34 @@
+from os import getenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from routes import IndexView
+from routes import AddBookView
+from entries import Database
+from routes import ListView
+from routes import EntryValidator
+#from sqlalchemy.sql import text
+
+app = Flask(__name__)
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
+
+db = SQLAlchemy(app)
+db_relay = Database(db)
+
+app.add_url_rule(
+    "/",
+    view_func=IndexView.as_view("index", "index.html"),
+)
+
+app.add_url_rule(
+    "/add_new_book",
+    view_func=AddBookView.as_view("add_new_book", db_relay, EntryValidator(), "add_new_book.html"),
+)
+
+app.add_url_rule(
+    "/list",
+    view_func=ListView.as_view("list", db_relay, "list.html"),
+)
---
+++ src/flaskapp/entries.py
@@ -0,0 +1,40 @@
+#import app
+from sqlalchemy.sql import text
+
+class Database():
+    def __init__(self, db) -> None:
+        self._db = db
+
+    def add_book(self, authors, title, publisher, year, isbn):
+        try:
+            sql = text("INSERT INTO citations (title,publisher,year,isbn) VALUES (:title, :publisher, :year, :isbn) RETURNING id")
+            citation_id = self._db.session.execute(sql, {"title":title, "publisher":publisher, "year":year, "isbn":isbn}).fetchone()[0]
+            
+            author_ids = []
+            for author in authors.splitlines():
+                author_ids.append(self._add_author(author))
+            for author_id in author_ids:
+                self._add_author_citation(author_id, citation_id)
+            self._db.session.commit()
+        except:
+            return False
+        return True
+
+    def _add_author(self, name):
+        sql = text("INSERT INTO authors (name) VALUES (:name) ON CONFLICT DO NOTHING")
+        self._db.session.execute(sql, {"name": name})
+        self._db.session.commit()
+        sql = text("SELECT id FROM authors WHERE name = :name")
+        result = self._db.session.execute(sql, {"name":name}).fetchone()[0]
+        return result
+        
+        
+    def _add_author_citation(self, author_id,citation_id):
+        sql = text("INSERT INTO authors_citations (author_id, citation_id) VALUES (:author_id, :citation_id)")
+        self._db.session.execute(sql, {"author_id":author_id, "citation_id":citation_id})
+        #app.db.session.commit()
+
+    def get_all_citations(self):
+        sql = text("SELECT * FROM citations")
+        result = self._db.session.execute(sql).fetchall()
+        return result
