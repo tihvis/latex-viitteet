5caca0cdc4cf7f3f5f0d9c537456072555dbb30b
Mira <mira.tihverainen@gmail.com>
2023-11-25 21:13:34 +0200

    new fix robot test script

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,10 +1,10 @@
-poetry run python3 src/flaskapp/app.py &
+poetry run invoke run &
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
   do sleep 1;
 done
 
-poetry run robot src/tests
+poetry run invoke robottest
 
 status=$?
 
--- tasks.py
+++ tasks.py
@@ -7,3 +7,12 @@ def init_db(ctx):
     engine = create_engine("postgresql://postgres:postgres@localhost:5432")
     with engine.begin() as cursor:
         cursor.execute(text(open("schema.sql", "r").read()))
+
+@task
+def run(ctx):
+    ctx.run("python src/flaskapp/app.py")
+
+@task
+def robot_test(ctx):
+    ctx.run("robot ./src/tests")
+
