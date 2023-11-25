c1f3c0bfd492493204e89a79fecb62e37362a90e
Mira <mira.tihverainen@gmail.com>
2023-11-25 20:49:58 +0200

    PG test

--- tasks.py
+++ tasks.py
@@ -4,6 +4,6 @@ from sqlalchemy import create_engine, text
 
 @task
 def init_db(ctx):
-    engine = create_engine("postgresql:///postgres")
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432")
     with engine.begin() as cursor:
         cursor.execute(text(open("schema.sql", "r").read()))
