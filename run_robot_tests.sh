a03a7f4286e5dd48a949c2bbd73a70dd47cde5b1
Mira <mira.tihverainen@gmail.com>
2023-11-25 21:00:44 +0200

    fix robot test script

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,13 +1,13 @@
+poetry run python3 src/flaskapp/app.py &
 
-start-pg.sh &
-cd src/flaskapp
-poetry run flask run &
-
-#while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
-#  do sleep 1;
-#done
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
+  do sleep 1;
+done
 
 poetry run robot src/tests
+
 status=$?
+
 kill $(lsof -t -i:5000)
+
 exit $status
