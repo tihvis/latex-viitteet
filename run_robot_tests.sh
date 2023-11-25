3c9059b4468f0c7f1ccc935e019b988bf398d3bf
Mira <mira.tihverainen@gmail.com>
2023-11-25 21:34:32 +0200

    fix

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -2,7 +2,7 @@ mv test.env .env
 
 poetry run flask --app src/flaskapp/app.py run &
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000)" != "200" ]];
   do sleep 1;
 done
 
