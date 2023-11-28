6c5b8f6b2fe7b0d6d661b69cf16db95d880a2e81
Riitta Pessi <riitta@pessi.fi>
2023-11-28 13:46:23 +0200

    Lisää Pylint-hinkkausta

--- src/flaskapp/routes.py
+++ src/flaskapp/routes.py
@@ -1,5 +1,5 @@
-from flask import flash, render_template, redirect, request, make_response
 import re
+from flask import flash, render_template, redirect, request, make_response
 from flask.views import View
 
 #
@@ -23,24 +23,24 @@ class AddBookView(View):
     def dispatch_request(self):
         if request.method == "GET":
             return render_template(self._template)
-        else:
-            #riku/ville? olisi näppärää, jos tämä tulisi sisäänkirjautumistietoilla?
-            type = "book"
-            title = str(request.form["title"])
-            author_list = str(request.form["author"])
-            isbn = str(request.form["isbn"])
-            year = str(request.form["year"])
-            publisher = str(request.form["publisher"])
-            #keywords_list = request.form["keywords"]
-            msg_tuple = self._validator.validate(author_list, title, year, publisher, isbn)
-            if msg_tuple[0] == False:
-                return render_template("error.html", error=msg_tuple[1])
-            self._database_relay.add_book(author_list, title, isbn, year, publisher)
-            flash("Lisäys onnistui!")
-            return redirect("/")      
-            #Tähän vielä joku tarkistus onko kyseistä kirjaa jo olemassa tietokannassa.
-            #Eli tarkistus onko sama isbn jo lisätty, tai onko sama otsikko+vuosi kombo jo olemassa
-            #     return render_template("error.html", error="Kyseinen kirja on jo lisätty tietokantaan, voit hakea lisäämäsi viitteet etusivulta.")
+        #riku/ville? olisi näppärää, jos tämä tulisi sisäänkirjautumistietoilla?
+        type = "book"
+        title = str(request.form["title"])
+        author_list = str(request.form["author"])
+        isbn = str(request.form["isbn"])
+        year = str(request.form["year"])
+        publisher = str(request.form["publisher"])
+        #keywords_list = request.form["keywords"]
+        msg_tuple = self._validator.validate(author_list, title, year, publisher, isbn)
+        if not msg_tuple[0]:
+            return render_template("error.html", error=msg_tuple[1])
+        self._database_relay.add_book(author_list, title, isbn, year, publisher)
+        flash("Lisäys onnistui!")
+        return redirect("/")
+        #Tähän vielä joku tarkistus onko kyseistä kirjaa jo olemassa tietokannassa.
+        #Eli tarkistus onko sama isbn jo lisätty, tai onko sama otsikko+vuosi kombo jo olemassa
+        #     return render_template("error.html", error="Kyseinen kirja on jo lisätty
+        # tietokantaan, voit hakea lisäämäsi viitteet etusivulta.")
 
 class AddArticleView(View):
     methods = ["GET", "POST"]
@@ -52,21 +52,21 @@ class AddArticleView(View):
     def dispatch_request(self):
         if request.method == "GET":
             return render_template(self._template)
-        else:
-            type = "article"
-            title = str(request.form["title"])
-            author_list = str(request.form["author"])
-            journal = str(request.form["journal"])
-            year = str(request.form["year"])
-            volume = str(request.form["volume"])
-            pages = str(request.form["pages"])
-            msg_tuple = self._validator.validate_article(author_list, title, journal, year, volume, pages)
-            if msg_tuple[0] == False:
-                return render_template("error.html", error=msg_tuple[1])
-            #self._database_relay.add_article(author_list, title, journal, year, volume, pages)
-            flash("Lisäys onnistui!")
-            return redirect("/")   
-        
+        type = "article"
+        title = str(request.form["title"])
+        author_list = str(request.form["author"])
+        journal = str(request.form["journal"])
+        year = str(request.form["year"])
+        volume = str(request.form["volume"])
+        pages = str(request.form["pages"])
+        msg_tuple = self._validator.validate_article(author_list, title, journal,
+             year, volume, pages)
+        if not msg_tuple[0]:
+            return render_template("error.html", error=msg_tuple[1])
+        #self._database_relay.add_article(author_list, title, journal, year, volume, pages)
+        flash("Lisäys onnistui!")
+        return redirect("/")
+
 class ListView(View):
     def __init__(self, db, template):
         self._db = db
@@ -90,13 +90,14 @@ class EntryValidator():
         pass
 
     def validate(self, author_list, title, year, publisher, isbn):
-        if not (1 <= len(title) <= 80):
+        if not 1 <= len(title) <= 80:
             return (False, "Kirjan otsikon tulee olla 1-80 merkkiä pitkä.")
         if not (5 <= len(isbn) <= 17) or not re.match("^[0-9-]+$", isbn):
-            return (False, "ISBN-koodin tulee olla 5-17 merkkiä pitkä, ja koostua vain numeroista ja viivoista.")
+            return (False,
+            "ISBN-koodin tulee olla 5-17 merkkiä pitkä, ja koostua vain numeroista ja viivoista.")
         if year == "" or not (1 <= int(year) <= 2025) or not year.isdigit():
             return (False, "Vuosiluku ei kelpaa.")
-        if not (2 <= len(publisher) <= 40):
+        if not 2 <= len(publisher) <= 40:
             return (False, "Kustantajan nimen tulee olla 2-40 merkkiä pitkä.")
         if len(author_list) == 0:
             return (False, "Viitteeseen tulee lisätä vähintään yksi kirjailija.")
@@ -105,14 +106,15 @@ class EntryValidator():
         #for author in author_list:
         #    names = author.split()
             #if len(names) < 2:
-            #    return render_template("error.html", error="Jokaisen kirjailijan nimessä tulee olla vähintään kaksi nimeä.")
+            #    return render_template("error.html", error="Jokaisen kirjailijan
+            #  nimessä tulee olla vähintään kaksi nimeä.")
 
     def validate_article(self, author_list, title, journal, year, volume, pages):
-        if not (1 <= len(title) <= 80):
+        if not 1 <= len(title) <= 80:
             return (False, "Artikkelin otsikon tulee olla 1-80 merkkiä pitkä.")
         if len(author_list) == 0:
             return (False, "Viitteeseen tulee lisätä vähintään yksi kirjailija.")
-        if not (1 <= len(journal) <= 80):
+        if not 1 <= len(journal) <= 80:
             return (False, "Lehden nimen tulee olla 1-80 merkkiä pitkä.")
         if year == "" or not (1 <= int(year) <= 2025) or not year.isdigit():
             return (False, "Vuosiluku ei kelpaa.")
@@ -121,4 +123,3 @@ class EntryValidator():
         if not re.match("^[0-9-]+$", pages):
             return (False, "Ilmoita sivunumerot muodossa 38-42.")
         return (True, "")
-
