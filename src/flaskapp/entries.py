8d2d9bfb4d36803af31eb6307da9e4cff4b2dc8d
Riitta Pessi <riitta@pessi.fi>
2023-11-28 14:28:32 +0200

    Pynlint-hinkattu, tarvitsee vielä except-errorin tyypin

--- src/flaskapp/entries.py
+++ src/flaskapp/entries.py
@@ -7,15 +7,17 @@ class Database():
 
     def add_book(self, authors, title, publisher, year, isbn):
         try:
-            sql = text("INSERT INTO citations (title,publisher,year,isbn) VALUES (:title, :publisher, :year, :isbn) RETURNING id")
-            citation_id = self._db.session.execute(sql, {"title":title, "publisher":publisher, "year":year, "isbn":isbn}).fetchone()[0]
+            sql = text("INSERT INTO citations (title,publisher,year,isbn) \
+                 VALUES (:title, :publisher, :year, :isbn) RETURNING id")
+            citation_id = self._db.session.execute(sql, {"title":title, "publisher":publisher,\
+                 "year":year, "isbn":isbn}).fetchone()[0]
             author_ids = []
             for author in authors.splitlines():
                 author_ids.append(self._add_author(author))
             for author_id in author_ids:
                 self._add_author_citation(author_id, citation_id)
             self._db.session.commit()
-        except:
+        except: #tää pitäis määritellä, lint: "No exception type(s) specified (bare-except)"
             return False
         return True
 
@@ -28,7 +30,8 @@ class Database():
         return result
 
     def _add_author_citation(self, author_id,citation_id):
-        sql = text("INSERT INTO authors_citations (author_id, citation_id) VALUES (:author_id, :citation_id)")
+        sql = text("INSERT INTO authors_citations (author_id, citation_id) \
+            VALUES (:author_id, :citation_id)")
         self._db.session.execute(sql, {"author_id":author_id, "citation_id":citation_id})
         #app.db.session.commit()
 
