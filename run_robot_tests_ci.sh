59b90950c39ba6f5d4af634f28bcc07a022cfd60
jussitus <juzzitus@protonmail.com>
2023-12-02 10:38:20 +0200

    Fix 3

--- run_robot_tests_ci.sh
+++ run_robot_tests_ci.sh
@@ -1,5 +1,8 @@
-export DATABASE_URL=postgresql://postgres:postgres@localhost:5432
+export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/test
 export SECRET_KEY=eb2d0dfe57915dd533d1021c6dab357a
+
+createdb -U postgres test
+psql -U postgres test < schema.sql
 
 poetry run flask --app src/flaskapp/app.py run &
 
@@ -14,6 +17,7 @@ status=$?
 
 kill $(lsof -t -i:5000)
 
+dropdb -U postgres test
 
 
 exit $status
