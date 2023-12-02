3ed6a9fa6fb7ccb40ff2b9d1a97b37c7ec8aa23d
jussitus <juzzitus@protonmail.com>
2023-12-02 11:07:27 +0200

    Fix 7

--- run_robot_tests_ci.sh
+++ run_robot_tests_ci.sh
@@ -1,8 +1,7 @@
 export DATABASE_URL=postgresql://postgres:postgres@localhost:5432
 export SECRET_KEY=eb2d0dfe57915dd533d1021c6dab357a
 
-psql -h localhost -U postgres < schema.sql
-
+psql postgresql://postgres:postgres@localhost:5432 < schema.sql
 poetry run flask --app src/flaskapp/app.py run &
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000)" != "200" ]];
