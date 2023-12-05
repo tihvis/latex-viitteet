0221f540bc32428e2f2bed4aac9e099d8ea6bc60
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2023-12-05 13:24:49 +0200

    Lisätty id user olioon

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,12 +1,20 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-    # id on flask_login moduulin luoma, saa kutsumalla User.get_id()
     """Käyttäjä luokka, sisältää käyttäjän id, nimen ja salasanan."""
-    def __init__(self, username, password_hash) -> None:
+    def __init__(self, id, username, password_hash) -> None:
+        self._id = id 
         self._username = username
         self._password_hash = password_hash
 
     @property
     def username(self):
         return self._username
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def get_id(self):
+        return self._id
