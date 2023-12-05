c27ca6f64954c39abe08c03bfd8c6e88fd6879c3
jussitus <juzzitus@protonmail.com>
2023-12-05 13:43:14 +0200

    Change schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,14 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username TEXT UNIQUE,
+	password_hash TEXT
+);
+
 CREATE TABLE citations (
 	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users ON DELETE CASCADE, 
 	bibtex JSONB
 );
+
 
 CREATE UNIQUE INDEX cite_ind_unique ON citations((bibtex ->> 'type'),(bibtex ->> 'title'),(bibtex ->> 'year'));
