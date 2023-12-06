1feb9e37e1a29ce4adb00d2a004090b7c8183f3f
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2023-12-06 18:58:05 +0200

    Lisätty käyttäjä repo db funktioita

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -2,7 +2,6 @@ from entities.user import User
 from sqlalchemy.sql import text
 
 class UserRepository():
-    # Tietokantaan pitää luoda käyttäjille oma osansa
     """Luokka käyttäjien lisäämistä, poistamista ja muokkausta varten tietokannassa"""
     def __init__(self, db) -> None:
         self._db = db
@@ -10,6 +9,8 @@ class UserRepository():
     def get_user_by_id_from_database(self, user_uuid : str):
         sql = text("SELECT uuid, username, password_hash FROM users WHERE uuid=:uuid")
         result = self._db.session.execute(sql, {"uuid" : user_uuid}).fetchall()[0]
+        if len(result) < 1:
+            return None
         return User(result[0], result[1], result[2])
 
     def create_user_in_database(self, user : User):
@@ -17,11 +18,13 @@ class UserRepository():
             sql = text("INSERT INTO users (uuid, username, password_hash) VALUES (:uuid, :username, :password_hash)")
             self._db.session.execute(sql, {"uuid": user.get_id(), "username": user.username, "password_hash": user.password_hash})
             self._db.session.commit()
-        except Exception as e:
-            raise e
+        except:
+            return False
         return True
 
     def delete_user_from_database(self, user : User):
+        #sql = text("DELETE FROM users WHERE uuid=:uuid RETURNING *")
+        #result = self._db.session.execute(sql, {"uuid" : user.get_id()}).fetchall()[0]
         pass
 
     def update_user_data(self, user : User):
@@ -29,9 +32,11 @@ class UserRepository():
 
     def is_username_taken(self, username : str):
         return False
-
-    def debug_dump_db(self):
-        sql = text("SELECT * FROM users")
-        result = self._db.session.execute(sql).fetchall()
-        #result = [r[0] for r in result]
-        return result
+    
+    def does_user_already_exist(self, user_uuid : str):
+        sql = text("SELECT uuid FROM users WHERE uuid=:uuid")
+        result = self._db.session.execute(sql, {"uuid" : user_uuid}).fetchall()[0]
+        if len(result) < 1:
+            return False
+        
+        return True
